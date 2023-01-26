05bb1d6e91fa0efe2078e0108e70359d413bca77
Thomas pvl <tp_vl@hotmail.com>
2023-01-26 11:45:16 -0300

    Script banco login
    script de conexão do banco criado tela de login iniciada

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $nome=$_POST['nome'];
+    $password=$_POST ['password'];
+    include("conectabdb.php");
+
+    $sql="SELECT COUNT(usu_id) FROM usuarios WHERE usu_nome='$nome' AND usu_senha='$password'";
+$resultado=mysqli_query($link.$sql);
+while($tbl=mysqli_fetch_array($resultado)){
+$cont=$tbl[0];
+}
+if ($cont==1) {
+    header("location: homesistema.html");   # code...
+}
+else {
+    echo"<script>window.alert('USUARIOS OU SENHA INCORRETOS!');</script>";
+
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN USUARIOS</title>
+    <link rel="stylesheet" href="./stylecadastra.css">
+</head>
+<body>
+    <div class="container">
+        <script>
+            function mostrarsenha() {
+               var tipo=document.getElementByid("senha");
+               if (tipo.type == "password") {
+                tipo.type="text";
+                
+               } 
+               else{
+                tipo.type="password";
+               }
+                
+            }
+        </script>
+        <form action="login.php" method="POST">
+            <h1>LOGIN DE USUARIO</h1>
+            <input type="text" name="nome" id="nome" placeholder="nome">
+            <p></p>
+            <input id="password" name="password" placeholder="Senha">
+            <img id="olinho" onclick="mostrarsenha()" src="assets/eye.svg">
+            <p></p>
+            <input type="submit" name="login" value="LOGIN">
+        </form>
+    </div>
+    
+</body>
+</html>
